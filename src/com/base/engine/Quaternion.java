e4253e83959ee65149025d14fff6fabede9b4596
Anders Gjerde <anders.gjerde@geomatikk-ikt.no>
2014-01-19 15:53:19 +0100

    #6 3D Game Engine Tutorial: Quaternions
    http://www.youtube.com/watch?v=GnKGZYcsJ3E&list=PLEETnX-uPtBXP_B2yupUKlflXBznWIlL5&index=7

---
+++ src/com/base/engine/Quaternion.java
@@ -0,0 +1,95 @@
+package com.base.engine;
+
+/**
+ * A lot like a four dimensional vector
+ * Quaternions have imaginary components (x, y and z)
+ * The w-component represents the "twirl" of the vector
+ * It makes rotation very easy, by using quaternion multiplication
+ */
+public class Quaternion {
+    private float x;  //imaginary
+    private float y;  //imaginary
+    private float z;  //imaginary
+    private float w;  //real (Twirling the marshmallow!)
+
+    public Quaternion(float x, float y, float z, float w) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+        this.w = w;
+    }
+
+    public float length() {
+        return (float) Math.sqrt(x * x + y * y + z * z + w * w);
+    }
+
+    public Quaternion normalize() {
+        float length = length();
+
+        x /= length;
+        y /= length;
+        z /= length;
+        w /= length;
+
+        return this;
+    }
+
+    /**
+     * Multiplies all the imaginary components by -1
+     * The real component (w) stays the same
+     * @return
+     */
+    public Quaternion conjugate() {
+        return new Quaternion(-x, -y, -z, w);
+    }
+
+    public Quaternion mul(Quaternion r) {
+        float w_ = w * r.getW() - x * r.getX() - y * r.getY() - z * r.getZ();
+        float x_ = x * r.getW() + w * r.getX() + y * r.getZ() - z * r.getY();
+        float y_ = y * r.getW() + w * r.getY() + z * r.getX() - x * r.getZ();
+        float z_ = z * r.getW() + w * r.getZ() + x * r.getY() - y * r.getX();
+
+        return new Quaternion(x_, y_, z_, w_);
+    }
+
+    public Quaternion mul(Vector3f r) {
+        float w_ = -x * r.getX() - y * r.getY() - z * r.getZ();
+        float x_ =  w * r.getX() + y * r.getZ() - z * r.getY();
+        float y_ =  w * r.getY() + z * r.getX() - x * r.getZ();
+        float z_ =  w * r.getZ() + x * r.getY() - y * r.getX();
+
+        return new Quaternion(x_, y_, z_, w_);
+    }
+
+    public float getX() {
+        return x;
+    }
+
+    public void setX(float x) {
+        this.x = x;
+    }
+
+    public float getY() {
+        return y;
+    }
+
+    public void setY(float y) {
+        this.y = y;
+    }
+
+    public float getZ() {
+        return z;
+    }
+
+    public void setZ(float z) {
+        this.z = z;
+    }
+
+    public float getW() {
+        return w;
+    }
+
+    public void setW(float w) {
+        this.w = w;
+    }
+}
