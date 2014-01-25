1334576a945cf1c9cb8edfb73b65a4de79c27308
Anders Gjerde <anders.gjerde@geomatikk-ikt.no>
2014-01-25 18:22:11 +0100

    #8 3D Game Engine Tutorial: Mesh Rendering - added comments
    http://www.youtube.com/watch?v=DJnzafkmuTA&list=PLEETnX-uPtBXP_B2yupUKlflXBznWIlL5

--- src/com/base/engine/Mesh.java
+++ src/com/base/engine/Mesh.java
@@ -17,19 +17,21 @@ public class Mesh {
         //size = vertices.length * Vertex.SIZE;
         size = vertices.length;
 
-        glBindBuffer(GL_ARRAY_BUFFER, vbo);
+        glBindBuffer(GL_ARRAY_BUFFER, vbo); //bind a named buffer object
         glBufferData(GL_ARRAY_BUFFER, Util.createFlippedBuffer(vertices), GL_STATIC_DRAW);
+                                //creates and initializes a buffer object's data store
                                 //GL_STATIC_DRAW - used to tell that we're
                                 //sending static drawing data
     }
 
     public void draw() {
         glEnableVertexAttribArray(0);
+            //Enable or disable a generic vertex attribute array
 
         glBindBuffer(GL_ARRAY_BUFFER, vbo);
         //multiplying by 4 because one float is 4 bytes
         glVertexAttribPointer(0, 3, GL_FLOAT, false, Vertex.SIZE * 4, 0);
-        glDrawArrays(GL_TRIANGLES, 0, size);
+        glDrawArrays(GL_TRIANGLES, 0, size); //render primitives from array data
 
         glDisableVertexAttribArray(0);
     }
