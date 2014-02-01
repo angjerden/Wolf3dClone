0958cebf2d08c8fdd7c0eb84dab1c1dc488b0527
Anders Gjerde <anders.gjerde@geomatikk-ikt.no>
2014-02-01 20:44:10 +0100

    #13 & 14 3D Game Engine Tutorial: Rotation & Scaling
    http://www.youtube.com/watch?v=OM7W9hZreRM
    http://www.youtube.com/watch?v=zz0_sXu6LkE

--- src/com/base/engine/Transform.java
+++ src/com/base/engine/Transform.java
@@ -3,21 +3,37 @@ package com.base.engine;
 import org.lwjgl.util.vector.*;
 
 /**
- * Should handle all our transformation processes
+ * Should handle all our transformation processes:
+ * - Translation
+ * - Rotation
+ * - Scale
+ *
+ * Wraps a Vector3f + all the transformative properties
+ * A way to represent this is through matrices.
  */
 public class Transform {
-    private Vector3f translation;
+    private Vector3f translation; //movement aligned with axes
+    private Vector3f rotation; //movement around axes
+    private Vector3f scale;
 
     public Transform() {
         translation = new Vector3f(0, 0, 0);
+        rotation = new Vector3f(0, 0, 0);
+        scale = new Vector3f(1, 1, 1);
     }
 
     public Matrix4f getTransformation() {
         Matrix4f translationMatrix = new Matrix4f().initTranslation(
                 translation.getX(), translation.getY(), translation.getZ()
         );
+        Matrix4f rotationMatrix = new Matrix4f().initRotation(
+                rotation.getX(), rotation.getY(), rotation.getZ()
+        );
+        Matrix4f scaleMatrix = new Matrix4f().initScale(
+                scale.getX(), scale.getY(), scale.getZ()
+        );
 
-        return translationMatrix;
+        return translationMatrix.mul(rotationMatrix.mul(scaleMatrix));
     }
 
     public Vector3f getTranslation() {
@@ -30,5 +46,29 @@ public class Transform {
 
     public void setTranslation(float x, float y, float z) {
         this.translation = new Vector3f(x, y, z);
+    }
+
+    public Vector3f getRotation() {
+        return rotation;
+    }
+
+    public void setRotation(Vector3f rotation) {
+        this.rotation = rotation;
+    }
+
+    public void setRotation(float x, float y, float z) {
+        this.rotation = new Vector3f(x, y, z);
+    }
+
+    public Vector3f getScale() {
+        return scale;
+    }
+
+    public void setScale(Vector3f scale) {
+        this.scale = scale;
+    }
+
+    public void setScale(float x, float y, float z) {
+        this.scale = new Vector3f(x, y, z);
     }
 }
