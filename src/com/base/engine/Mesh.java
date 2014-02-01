7c7189b48d4a7be6b6f19d623d574d144ceceedb
Anders Gjerde <anders.gjerde@geomatikk-ikt.no>
2014-02-02 00:20:41 +0100

    #15 3D Game Engine Tutorial: Mesh Faces
    http://www.youtube.com/watch?v=JBGsM-ANnDw

--- src/com/base/engine/Mesh.java
+++ src/com/base/engine/Mesh.java
@@ -5,23 +5,31 @@ import static org.lwjgl.opengl.GL15.*;
 import static org.lwjgl.opengl.GL20.*;
 
 public class Mesh {
-    private int vbo; //a pointer to a buffer
+    private int vbo; //a pointer to a buffer (vertex buffer object)
+    private int ibo; //index buffer object
     private int size; //how many bytes of data we have
 
     public Mesh() {
         vbo = glGenBuffers(); //generate buffer object names
+        ibo = glGenBuffers();
         size = 0;
     }
 
-    public void addVertices(Vertex[] vertices) {
-        //size = vertices.length * Vertex.SIZE;
-        size = vertices.length;
+    public void addVertices(Vertex[] vertices, int[] indices) {
+        size = indices.length;
 
         glBindBuffer(GL_ARRAY_BUFFER, vbo); //bind a named buffer object
-        glBufferData(GL_ARRAY_BUFFER, Util.createFlippedBuffer(vertices), GL_STATIC_DRAW);
+        glBufferData(GL_ARRAY_BUFFER,
+                Util.createFlippedBuffer(vertices),
+                GL_STATIC_DRAW);
                                 //creates and initializes a buffer object's data store
                                 //GL_STATIC_DRAW - used to tell that we're
                                 //sending static drawing data
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo);
+        glBufferData(GL_ELEMENT_ARRAY_BUFFER,
+                       Util.createFlippedBuffer(indices),
+                       GL_STATIC_DRAW);
+
     }
 
     public void draw() {
@@ -31,7 +39,9 @@ public class Mesh {
         glBindBuffer(GL_ARRAY_BUFFER, vbo);
         //multiplying by 4 because one float is 4 bytes
         glVertexAttribPointer(0, 3, GL_FLOAT, false, Vertex.SIZE * 4, 0);
-        glDrawArrays(GL_TRIANGLES, 0, size); //render primitives from array data
+
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo);
+        glDrawElements(GL_TRIANGLES, size, GL_UNSIGNED_INT, 0);
 
         glDisableVertexAttribArray(0);
     }
--- src/com/base/engine/Util.java
+++ src/com/base/engine/Util.java
@@ -3,6 +3,7 @@ package com.base.engine;
 import org.lwjgl.BufferUtils;
 
 import java.nio.FloatBuffer;
+import java.nio.IntBuffer;
 
 /**
  * Java stores buffers differently than OpenGL
@@ -17,6 +18,18 @@ public class Util {
      */
     public static FloatBuffer createFloatBuffer(int size) {
         return BufferUtils.createFloatBuffer(size);
+    }
+
+    public static IntBuffer createIntBuffer(int size) {
+        return BufferUtils.createIntBuffer(size);
+    }
+
+    public static IntBuffer createFlippedBuffer(int... values) {
+        IntBuffer buffer = createIntBuffer(values.length);
+        buffer.put(values);
+        buffer.flip();
+
+        return buffer;
     }
 
     public static FloatBuffer createFlippedBuffer(Vertex[] vertices) {
