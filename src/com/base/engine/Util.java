a926d3990a596118e86dfc3b1808c7af2751745b
Anders Gjerde <anders.gjerde@geomatikk-ikt.no>
2014-02-02 15:46:04 +0100

    #16 3D Game Engine Tutorial: Mesh Loading (OBJ)
    http://www.youtube.com/watch?v=8LJU70o8QXs

--- src/com/base/engine/Util.java
+++ src/com/base/engine/Util.java
@@ -4,6 +4,7 @@ import org.lwjgl.BufferUtils;
 
 import java.nio.FloatBuffer;
 import java.nio.IntBuffer;
+import java.util.ArrayList;
 
 /**
  * Java stores buffers differently than OpenGL
@@ -58,5 +59,27 @@ public class Util {
         buffer.flip();
 
         return buffer;
+    }
+
+    public static String[] removeEmptyStrings(String[] data) {
+        ArrayList<String> result = new ArrayList<String>();
+
+        for (int i = 0; i < data.length; i++) {
+            if (!data[i].equals("")) {
+                result.add(data[i]);
+            }
+        }
+        String[] res = new String[result.size()];
+        result.toArray(res);
+        return res;
+    }
+
+    public static int[] toIntArray(Integer[] data) {
+        int[] result = new int[data.length];
+
+        for (int i = 0; i < data.length; i++) {
+            result[i] = data[i].intValue();
+        }
+        return result;
     }
 }
