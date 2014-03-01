0c2470b958369ff28376a934c1e4cf1999cf5b75
Anders Gjerde <anders.gjerde@geomatikk-ikt.no>
2014-03-01 17:09:32 +0100

    #1-3 Wolfenstein 3D Clone Tutorial: Levels and Rendering

---
+++ src/com/base/engine/Bitmap.java
@@ -0,0 +1,55 @@
+package com.base.engine;
+
+
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOError;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
+public class Bitmap {
+    private int width;
+    private int height;
+    private int[] pixels;
+
+    public Bitmap(String fileName) {
+        try{
+            BufferedImage image = ImageIO.read(new File("./res/bitmaps/" + fileName));
+            width = image.getWidth();
+            height = image.getHeight();
+
+            pixels = new int[width * height];
+            image.getRGB(0, 0, width, height, pixels, 0, width);
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+
+    }
+
+    public Bitmap(int width, int height) {
+        this.width = width;
+        this.height = height;
+        this.pixels = new int[width * height];
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public int[] getPixels() {
+        return pixels;
+    }
+
+    public int getPixel(int x, int y) {
+        return pixels[x + y * width];
+    }
+
+    public void setPixel(int x, int y, int value) {
+        pixels[x + y * width] = value;
+    }
+}
--- src/com/base/engine/Game.java
+++ src/com/base/engine/Game.java
@@ -5,146 +5,54 @@ import org.lwjgl.input.Keyboard;
 
 public class Game {
 
-    private Mesh mesh;
-    private Shader shader;
-    private Transform transform;
-    private Material material;
-    private Camera camera;
-
-    PointLight pLight1 = new PointLight(
-            new BaseLight(
-                    new Vector3f(1, 0.5f, 0),
-                    0.8f),
-            new Attenuation(0, 0, 1),
-            new Vector3f(-2, 0, 5f),
-            10);
-    PointLight pLight2 = new PointLight(
-                    new BaseLight(
-                            new Vector3f(0, 0.5f, 1),
-                            0.8f),
-                    new Attenuation(0, 0, 1),
-                    new Vector3f(2, 0, 7f),
-                    10);
-
-    SpotLight sLight1 = new SpotLight(
-            new PointLight(
-                new BaseLight(
-                        new Vector3f(0, 1f, 1f),
-                        0.8f),
-                new Attenuation(0, 0, 0.1f),
-                new Vector3f(-2, 0, 5f),
-                30),
-            new Vector3f(1, 1, 1),
-            0.7f);
+    Bitmap level;
+    Shader shader;
+    Material material;
+    Mesh mesh;
+    Transform transform;
 
     public Game() {
-        //mesh = new Mesh("box.obj");
-        material = new Material(new Texture("test.png"),
-                new Vector3f(1, 1, 1), 1, 8);
-        shader = PhongShader.getInstance();
-        camera = new Camera();
-        transform = new Transform();
+        level = new Bitmap("level1.png");
 
-/*        Vertex[] vertices = new Vertex[] {
-                new Vertex(new Vector3f(-1, -1, 0), new Vector2f(0, 0)),
-                new Vertex(new Vector3f(0, 1, 0), new Vector2f(0.5f, 0)),
-                new Vertex(new Vector3f(1, -1, 0), new Vector2f(1.0f, 0)),
-                new Vertex(new Vector3f(0, -1, 1), new Vector2f(0.0f, 0.5f))};
+        for (int i = 0; i < level.getWidth(); i++) {
+            for (int j = 0; j < level.getHeight(); j++) {
+                System.out.print(level.getPixel(i, j));
+            }
+            System.out.println();
+        }
 
-        int[] indices = new int[] {3, 1, 0,
-                                    2, 1, 3,
-                                    0, 1, 2,
-                                    0, 2, 3};*/
-
-
-/*        Vertex[] vertices = new Vertex[] {
-                new Vertex( new Vector3f(-1.0f, -1.0f, 0.5773f), new Vector2f(0.0f, 0.0f)),
-                new Vertex( new Vector3f(0.0f, -1.0f, -1.15475f), new Vector2f(0.5f, 0.0f)),
-                new Vertex( new Vector3f(1.0f, -1.0f, 0.5773f),	new Vector2f(1.0f, 0.0f)),
-                new Vertex( new Vector3f(0.0f, 1.0f, 0.0f), new Vector2f(0.5f, 1.0f)) };
-
-      	int indices[] = { 0, 3, 1,
-      	                  1, 3, 2,
-      		              2, 3, 0,
-      		              1, 2, 0 };*/
-
-        float fieldDepth = 10.0f;
-        float fieldWidth = 10.0f;
+        shader = BasicShader.getInstance();
+        material = new Material(new Texture("test.png"));
 
         Vertex[] vertices = new Vertex[] {
-                        new Vertex( new Vector3f(-fieldWidth, 0.0f, -fieldDepth), new Vector2f(0.0f, 0.0f)),
-                        new Vertex( new Vector3f(-fieldWidth, 0.0f, fieldDepth * 3), new Vector2f(0.0f, 1.0f)),
-                        new Vertex( new Vector3f(fieldWidth * 3, 0.0f, -fieldDepth),	new Vector2f(1.0f, 0.0f)),
-                        new Vertex( new Vector3f(fieldWidth * 3, 0.0f, fieldDepth * 3), new Vector2f(1.0f, 1.0f)) };
+                new Vertex(new Vector3f(0, 0, 0), new Vector2f(0, 0)),
+                new Vertex(new Vector3f(0, 1, 0), new Vector2f(0, 1)),
+                new Vertex(new Vector3f(1, 1, 0), new Vector2f(1, 1)),
+                new Vertex(new Vector3f(1, 0, 0), new Vector2f(1, 0))
+        };
 
-              	int indices[] = { 0, 1, 2,
-              	                  2, 1, 3};
+        int[] indices = new int[] {0, 1, 2,
+                                    0, 2, 3};
 
-
-        mesh = new Mesh(vertices, indices, true);
-
-        transform.setCamera(camera);
-        transform.setProjection(70f,
-                Window.getWidth(),
-                Window.getHeight(),
-                0.1f,
-                1000);
-        PhongShader.setAmbientLight(new Vector3f(0.1f, 0.1f, 0.1f));
-        PhongShader.setDirectionalLight(
-                new DirectionalLight(
-                            new BaseLight(new Vector3f(1, 1, 1), 0.8f),
-                            new Vector3f(1, 1, 1)
-                    ));
-
-        PhongShader.setPointLights(new PointLight[] {pLight1, pLight2});
-        PhongShader.setSpotLights(new SpotLight[] {sLight1});
+        mesh = new Mesh(vertices, indices);
+        transform = new Transform();
+        Transform.setCamera(new Camera());
+        Transform.setProjection(70, Window.getWidth(), Window.getHeight(), 0.01f, 1000f);
     }
 
     public void input() {
-        camera.input();
-/*        if(Input.getKeyDown(Keyboard.KEY_UP)) {
-            System.out.println("We've just pressed up!");
-        }
 
-        if(Input.getKeyUp(Keyboard.KEY_UP)) {
-            System.out.println("We've just released up!");
-        }
-
-        if(Input.getMouseDown(1)) {
-            System.out.println("We've just right clicked at " + Input.getMousePosition());
-        }
-        if(Input.getMouseUp(1)) {
-            System.out.println("We've just released right mouse button!");
-        }*/
     }
 
-    float temp = 0.0f;
-
     public void update() {
-        temp += Time.getDelta();
-
-        float sinTemp = (float) Math.sin(temp);
-
-        transform.setTranslation(0, -1, 5);
-        //transform.setRotation(0, sinTemp * 180, 0);
-        //transform.setScale(0.7f * sinTemp, 0.7f * sinTemp, 0.7f * sinTemp);
-
-        pLight1.setPosition(new Vector3f(3,
-                0,
-                8.0f * (float)(Math.sin(temp) + 1.0/2.0) + 10));
-        pLight2.setPosition(new Vector3f(7,
-                        0,
-                        8.0f * (float)(Math.cos(temp) + 1.0/2.0) + 10));
-
-        sLight1.getPointLight().setPosition(camera.getPos());
-        sLight1.setDirection(camera.getForward());
+        Transform.getCamera().input();
     }
 
     public void render() {
-        RenderUtil.setClearColor(Transform.getCamera().getPos().div(1024f).abs());
         shader.bind();
         shader.updateUniforms(transform.getTransformation(),
-                transform.getProjectedTransformation(), material);
+                transform.getProjectedTransformation(),
+                material);
         mesh.draw();
     }
 }
